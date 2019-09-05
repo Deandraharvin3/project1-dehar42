3e3a2438ddaf01fa29b6adb481d27e347aff0e48
Ubuntu <ubuntu@ip-172-31-33-191.ec2.internal>
2019-09-05 22:49:44 +0000

    Making the site look better

--- Artist.py
+++ Artist.py
@@ -14,10 +14,15 @@ def GetData():
 
     twitter_response = Twitter.ApiCall()
     random_num = random.randint(0, len(twitter_response["statuses"])-1)
-    twitter_path = twitter_response["statuses"][random_num]["text"]
+    twitter_path = twitter_response["statuses"][random_num]
     
     
-    f = flask.render_template("index.html", song_title=genius_path["full_title"], image=genius_path["song_art_image_url"], url=genius_path["url"], tweet=twitter_path)
-    return (f)
+    return flask.render_template("index.html", 
+    song_title=genius_path["full_title"], 
+    image=genius_path["song_art_image_url"], 
+    url=genius_path["url"], 
+    tweet=twitter_path["text"],
+    profile_photo=twitter_path["user"]["profile_image_url"],
+    username=twitter_path["user"]["screen_name"])
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
